c254410c847ef5e694fec20358890dedb456b22e
exfriend <i@zlad.tk>
2018-10-09 17:47:42 +0300

    fix

--- software/cli/fish/functions/w.fish
+++ software/cli/fish/functions/w.fish
@@ -1,3 +1,4 @@
+# Defined in /tmp/fish.xtyB35/w.fish @ line 1
 function w
-	./compose.fish exec --user=root {$argv}__workspace bash
+	./compose.fish exec --user=laradock {$argv}__workspace bash
 end
