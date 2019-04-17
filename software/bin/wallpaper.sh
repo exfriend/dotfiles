e8a8523dbd102c34aa7a4e6d80efcaff840ae108
librevlad <librevlad@gmail.com>
2019-04-17 17:52:40 +0300

    wip

--- software/bin/wallpaper.sh
+++ software/bin/wallpaper.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 sleep 3; 
-feh --bg-fill ~/dotfiles/excloud/wallpapers/blur-bg-blurred.jpg
+#feh --bg-fill ~/dotfiles/excloud/wallpapers/solid.png
+#feh --bg-fill ~/dotfiles/excloud/wallpapers/blur.jpg
+feh --bg-fill ~/dotfiles/excloud/wallpapers/11.jpg
