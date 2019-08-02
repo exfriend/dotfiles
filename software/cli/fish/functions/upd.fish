5877fcd8ae0c99f1856851fb960bf55bcfc778ab
librevlad <librevlad@gmail.com>
2019-08-02 13:48:51 +0300

    wip

--- software/cli/fish/functions/upd.fish
+++ software/cli/fish/functions/upd.fish
@@ -1,3 +1,4 @@
+# Defined in /tmp/fish.j5Ajsy/upd.fish @ line 2
 function upd
-   sudo apt full-upgrade; sudo apt autoremove; sudo apt autoclean; sudo apt clean
+	sudo apt update; sudo apt full-upgrade; sudo apt autoremove; sudo apt autoclean; sudo apt clean; purge-old-kernels
 end
