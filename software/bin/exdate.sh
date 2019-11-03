5a3fae2144a73eb1a842e80b34122798ca5f2ac0
Your Name <you@example.com>
2019-11-03 02:15:26 +0200

    wip

--- software/bin/exdate.sh
+++ software/bin/exdate.sh
@@ -2,7 +2,7 @@
 while true; do 
 
 
- echo $(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | php -r 'preg_match("~[0-9]*%~ims",file_get_contents("php://stdin"),$o); echo $o[0];') / $(date '+%d-%m-%Y %H:%M:%S')
+ echo $(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | php -r 'preg_match("~[0-9]*%~ims",file_get_contents("php://stdin"),$o); echo $o[0] ? $o[0] . ' /' : '';') $(date '+%d-%m-%Y %H:%M:%S')
  sleep 1
 
 done
--- workstations/exstudio/arandr.sh
+++ workstations/exstudio/arandr.sh
@@ -1,2 +1,2 @@
 #!/bin/sh
-xrandr --output DVI-D-1-1 --off --output VIRTUAL1 --off --output DP1 --mode 1440x900 --pos 2944x0 --rotate left --output HDMI-1-3 --off --output HDMI2 --mode 1280x1024 --pos 0x80 --rotate right --output HDMI1 --mode 1920x1080 --pos 1024x672 --rotate normal --output DP-1-4 --off --output DP-1-3 --off --output DP-1-2 --off
+xrandr --output DVI-D-0 --off --output HDMI-1-2 --mode 1280x1024 --pos 1360x0 --rotate normal --output HDMI-1-1 --mode 1360x768 --pos 0x0 --rotate normal --output DP-1-1 --mode 1440x900 --pos 4560x0 --rotate normal --output HDMI-0 --mode 1920x1080 --pos 2640x0 --rotate normal --output DP-5 --off --output DP-4 --off --output DP-3 --off --output DP-2 --off --output DP-1 --off --output DP-0 --off
