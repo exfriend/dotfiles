8295aab9836e8be5036fe5bce48bf5a5cdea8470
librevlad <librevlad@gmail.com>
2021-08-31 19:39:05 +0300

    wip

--- software/bin/exdate.sh
+++ software/bin/exdate.sh
@@ -1,8 +1,7 @@
 #!/bin/bash
 while true; do 
 
-
- echo $(upower -i /org/freedesktop/UPower/devices/battery_BAT0 | grep percentage | php -r 'preg_match("~[0-9]*%~ims",file_get_contents("php://stdin"),$o); echo $o[0] ? $o[0] . ' /' : '';') $(date '+%d-%m-%Y %H:%M:%S')
- sleep 1
+  php ~/dotfiles/software/bin/exdate.php
+  sleep 1
 
 done
